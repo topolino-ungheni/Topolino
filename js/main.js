5b57fa9f28002e2de1499455a9b38985d6c43530
topolino-ungheni <126409463+topolino-ungheni@users.noreply.github.com>
2023-02-26 19:28:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+new SimpleLightbox({ elements: '.container-galerie a' });
+new SimpleLightbox({ elements: '.serv-galerie a'})
+
+AOS.init({
+    duration: 900,
+    offset: 100,
+    once: true
+});
+
+emailjs.init("J-guAhO4A7ZJEkgWK");
+
+function mobile() {
+    var x = document.getElementById("nav");
+
+    if (x.className === "navbar") {
+        x.className = "navbar mobile";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_rguiedf", "template_smycmto", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+}
