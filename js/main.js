1e59ded2d0fd59c35c327ed77273c33b33f297b2
topolino-ungheni <126409463+topolino-ungheni@users.noreply.github.com>
2023-04-17 10:19:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,6 +19,24 @@ function mobile() {
     }
 }
 
+function onload() {
+    getYear();
+    console.log('page loaded');
+    // setScrollListener();
+}
+
+function getYear() {
+    var d = new Date(),
+        date = d.getDate(),
+        mon = d.getMonth(),
+        yr = d.getFullYear();
+    // var days = ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă"];
+    // document.getElementById("year").innerHTML = ((date < 10) ? '0' + date : date) + "/" +
+    document.getElementById("year").innerHTML = ('0' + date).slice(-2) + "/" +
+        // (((mon + 1) < 10) ? '0' + (mon + 1) : (mon + 1)) + "/" + yr + " " + days[d.getDay()];
+        ('0' + (mon + 1)).slice(-2) + "/" + yr;
+}
+
 function send() {
 
     if (document.getElementById("formular").checkValidity() == false) {
