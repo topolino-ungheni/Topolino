0b1c2f22ffc21db30979a909f2185a16172acb9a
topolino-ungheni <126409463+topolino-ungheni@users.noreply.github.com>
2024-01-07 13:09:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 new SimpleLightbox({ elements: '.container-galerie a' });
-new SimpleLightbox({ elements: '.serv-galerie a'})
+new SimpleLightbox({ elements: '.serv-galerie a' })
 
 AOS.init({
     duration: 900,
@@ -19,6 +19,24 @@ function mobile() {
     }
 }
 
+var menu = document.querySelector(".menu")
+var header = document.querySelector(".menu-nav")
+
+
+menu.addEventListener("click", function () {
+    header.classList.add("nav-mobile")
+})
+
+// function hamb_mobile() {
+//     var x = document.getElementById("hamb");
+
+//     if (x.className === "hamburger") {
+//         x.className = "hamburger mobile";
+//     } else {
+//         x.className = "hamburger";
+//     }
+// }
+
 function onload() {
     getYear();
     console.log('page loaded');
@@ -36,6 +54,68 @@ function getYear() {
         // (((mon + 1) < 10) ? '0' + (mon + 1) : (mon + 1)) + "/" + yr + " " + days[d.getDay()];
         ('0' + (mon + 1)).slice(-2) + "/" + yr;
 }
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "username",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
+
+function send2() {
+
+    if (document.getElementById("rezervare").checkValidity() == false) {
+        document.getElementById("rezervare").reportValidity();
+        return;
+    }
+
+    var datal = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        nr_telefon: document.getElementById("nr_telefon").value,
+        data: document.getElementById("data").value,
+        ora_sosirii: document.getElementById("ora_sosirii").value,
+        ora_plecarii: document.getElementById("ora_plecarii").value,
+        copii: document.getElementById("copii").value
+    };
+
+    emailjs.send("service_rguiedf", "template_8y9hhja", datal)
+        .then(function (raspuns) {
+            btn.value = 'SEND MESSAGE';
+            alert("Mesajul a fost transmis. Operatorul o să vă contacteze în decurs de câteva ore.")
+            document.getElementById("rezervare").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+}
+
+// const btn = document.getElementById('button');
+
+// document.getElementById('form')
+//  .addEventListener('submit', function(event) {
+//    event.preventDefault();
+
+//    btn.value = 'Sending...';
+
+//    const serviceID = 'service_rguiedf';
+//    const templateID = 'template_8y9hhja';
+
+//    emailjs.sendForm(serviceID, templateID, this)
+//     .then(() => {
+//       btn.value = 'Rezervează!';
+//       alert('Sent!');
+//     }, (err) => {
+//       btn.value = 'Rezervează!';
+//       alert(JSON.stringify(err));
+//     });
+// });
 
 function send() {
 
